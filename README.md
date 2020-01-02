0b6aba73083edc2f56fb820e389a3919d34ac8f9
deceptionlogic <phillip@deceptionlogic.com>
2020-01-02 17:03:21 -0500

    Initial code commit

--- README.md
+++ README.md
@@ -1,2 +1,72 @@
-# Splunk_TA_deceptionlogic
-The Deception Logic Add-on for Splunk!
+# Splunk TA for Deception Logic
+
+This Splunk App pulls event and alert data from the Deception Logic API.
+
+## Install
+
+Place this app on your search head under `$SPLUNK_HOME/etc/apps/`
+Create the index on your indexer, see step #2 under Prerequisite for details.
+
+## Configure
+
+In order for the app to pull data from Deception Logic you must add your API keys to the configuration. There are two files used to configure the app:
+
+- `bin/deceptionlogic.json`
+- `default/inputs.conf`
+
+At minimum, and recommended, you only need to add your Deception Logic API key ID and secret key to `bin/deceptionlogic.json`.
+
+__Configuration file: `bin/deceptionlogic.json`__
+
+    ```json
+    {
+        "X-DeceptionLogic-KeyId": "<your key ID>",
+        "X-DeceptionLogic-SecretKey": "<your secret key>",
+        "eventapi_run_time": "1m",
+        "alertapi_run_time": "30s"
+    }
+    ```
+
+Additional options:
+
+- The `eventapi_run_time` option is used to define the duration of event data to be extracted from Deception Logic. You must specify `<number><unit>`. For example, "1m" indicates one minute. Valid units are "d" (days), "h" (hours), "m" (minutes), and "s" (seconds).
+
+- The `alertapi_run_time` option is used to define the duration of the alert data to be extracted from Deception Logic. You must specify `<number><unit>`. For example, "1m" indicates one minute. Valid units are "d" (days), "h" (hours), "m" (minutes), and "s" (seconds).
+
+__Note:__ While you can modify the values of the `eventapi_run_time` and `alertapi_run_time` options, it is recommended to use the default values.
+
+If you change the duration options above, you will also need to change the interval values in `default/inputs.conf`. In order to avoid dupilicate or missing data issues, the duration options must match with the coorosponding interval values.
+
+## Creat Indexes
+
+Create indexes.conf on your indexer with the default index name "Deceptionlogic" Below is the sample of index:
+
+    ```conf
+    [deceptionlogic]
+    homePath   = $SPLUNK_DB/deceptionlogic/db
+    coldPath   = $SPLUNK_DB/deceptionlogic/colddb 
+    thawedPath = $SPLUNK_DB/deceptionlogic/thaweddb
+    #1 day retention 
+    frozenTimePeriodInSecs = 86400
+    #14 day retention
+    #frozenTimePeriodInSecs = 1209600
+    ```
+
+__**NOTE:__ If you change the index name, make sure you update `default/inputs.conf` to reflect the new index name, e.g. `index = <new index name >`
+
+__**NOTE:__ If you modify any files in the `default` folder, please create the copy of the file in local directory and change it in local directory. After completing changes, restart the Splunk process.
+
+## Viewing data in Splunk
+
+index="deceptionlogic" sourcetype="deceptionlogic_alerts"
+index="deceptionlogic" sourcetype="deceptionlogic_events"
+
+_If you changed indexname or sourcetype, please modify the above query accordingly._
+
+## Troubleshooting
+
+- You can view Splunk app error messages by querying `index=_internal source="*splunk/deceptionlogic.log"` or `index=_internal source = *splunkd.log`
+
+## Dashboard
+
+1. Select Splunk_TA_deceptionlogic app in the Splunk UI. Go to Dashboards and click on Deception Logic Events.
---
+++ bin/spl_deceptionlogic_events.py
@@ -0,0 +1,117 @@
+'''
+Send Deception Logic events to Splunk
+'''
+import os
+import sys
+import json
+import logging
+import logging.handlers
+import requests
+
+
+def setup_logger(level):
+    '''
+    WRITE THE INTERNAL LOGS TO LOGFILE FOR DECEPTIONLOGIC
+    '''
+    logger = logging.getLogger('')
+    logger.propagate = False # Prevent the log messages from being duplicated in the python.log file
+    logger.setLevel(level)
+    log_file = os.path.join(sys.path[0], "..", "..", "..", "..", 'var', 'log', 'splunk', 'deceptionlogic.log')
+    file_handler = logging.handlers.RotatingFileHandler(log_file, maxBytes=25000000, backupCount=5)
+    formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+    file_handler.setFormatter(formatter)
+
+    logger.addHandler(file_handler)
+    return logger
+
+### MAIN FUNCTION ###
+
+if __name__ == "__main__":
+
+    ## CHECK For the deceptionlogic.json file exists ##
+    filename = "deceptionlogic.json"
+    jsonfile = os.path.join(sys.path[0], filename)
+
+    try:
+        with open(jsonfile, 'r') as argfile:
+            data = argfile.read()
+    except:
+        logger = setup_logger(logging.ERROR)
+        logger.error("Events Error: Deceptionlogic args file missing : ./%s ", jsonfile)
+        exit()
+
+    # parse file
+    try:
+        args = json.loads(data)
+    except ValueError as jsonerror:
+        logger = setup_logger(logging.ERROR)
+        logger.error("Events Error: File %s data read error %s ", jsonfile, jsonerror)
+        exit()
+
+    if ("X-DeceptionLogic-KeyId" in args) and ("X-DeceptionLogic-SecretKey" in args):
+        keyId = str(args['X-DeceptionLogic-KeyId'])
+        SecretKey = str(args['X-DeceptionLogic-SecretKey'])
+    else:
+        logger = setup_logger(logging.ERROR)
+        logger.error("Events Error: Deceptionlogic args X-DeceptionLogic-KeyId OR/AND X-DeceptionLogic-SecretKey missing in file : ./%s ", jsonfile)
+        exit()
+
+    if (keyId and SecretKey):
+        headers = {
+            'X-DeceptionLogic-KeyId': keyId,
+            'X-DeceptionLogic-SecretKey': SecretKey,
+        }
+
+        response = requests.get('https://api.deceptionlogic.com/v0/authenticate', headers=headers, verify=False)
+        if response.status_code == 200:
+            logger = setup_logger(logging.INFO)
+            logger.info("Events INFO : DeceptionLogic API Authentication Success")
+        else:
+            logger = setup_logger(logging.ERROR)
+            logger.error("Events Error: DeceptionLogic API Authentication Failed. Response Code:%s ", response.status_code)
+            exit()
+
+        auth = response.json()
+        token = auth['token']
+        apid_id = auth['id']
+
+        headers = {
+            'X-DeceptionLogic-Token': token,
+            'X-DeceptionLogic-Id': apid_id,
+        }
+
+        if "eventapi_run_time" in args:
+            runtime = str(args['eventapi_run_time'])
+
+        if runtime is None:
+            runtime = '1m' ## Default value of runtime 1m
+
+        if runtime[-1] == "m" or runtime[-1] == "s" or  runtime[-1] == "h" or runtime[-1] == "d":
+            if runtime[:-1].isdigit():
+                url = 'https://api.deceptionlogic.com/v0/events?from={0}'.format(runtime)
+                response = requests.get(url, headers=headers, verify=False)
+            else:
+                logger = setup_logger(logging.ERROR)
+                logger.error("Events Error: Please enter the eventapi_run_time in deceptionlogic.json in right format (digits + m for months, d for days, s for seconds and h for hours). E.g for 1 min : 1m")
+                exit()
+        else:
+            logger = setup_logger(logging.ERROR)
+            logger.error("Events Error: Please enter the eventapi_run_time in deceptionlogic.json in right format (digits + m for months, d for days, s for seconds and h for hours). E.g for 1 min : 1m")
+            exit()
+
+        try:
+            eventjson = response.json()
+            if eventjson:
+                for i in eventjson:
+                    ### Send Data to Splunk ###
+                    data_j = json.dumps(i)
+                    print data_j
+
+        except ValueError:
+            logger = setup_logger(logging.ERROR)
+            logger.error("Events API call failed . Please check your authentication key or check with DeceptionLogic Support team. API response code : %s", response.status_code)
+            exit()
+    else:
+        logger = setup_logger(logging.ERROR)
+        logger.error("DeceptionLogic API key ID and Secret Key can not be blank. Please Enter the right keys")
+        exit()
