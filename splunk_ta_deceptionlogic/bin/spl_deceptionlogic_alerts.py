806a9337598dcbfcd4af3a9358e95471069da483
deceptionlogic <info@deceptionlogic.com>
2023-08-14 08:17:27 -0400

    linting

--- splunk_ta_deceptionlogic/bin/spl_deceptionlogic_alerts.py
+++ splunk_ta_deceptionlogic/bin/spl_deceptionlogic_alerts.py
@@ -1,6 +1,6 @@
-'''
+"""
 Send Deception Logic alerts to Splunk
-'''
+"""
 import os
 import sys
 import json
@@ -10,31 +10,45 @@ import requests
 
 
 def setup_logger(level):
-    '''
+    """
     WRITE THE INTERNAL LOGS TO LOGFILE FOR DECEPTIONLOGIC
-    '''
-    logger = logging.getLogger('')
-    logger.propagate = False # Prevent the log messages from being duplicated in the python.log file
+    """
+    logger = logging.getLogger("")
+    logger.propagate = (
+        False  # Prevent the log messages from being duplicated in the python.log file
+    )
     logger.setLevel(level)
-    log_file = os.path.join(sys.path[0], "..", "..", "..", "..", 'var', 'log', 'splunk', 'deceptionlogic.log')
-    file_handler = logging.handlers.RotatingFileHandler(log_file, maxBytes=25000000, backupCount=5)
-    formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+    log_file = os.path.join(
+        sys.path[0],
+        "..",
+        "..",
+        "..",
+        "..",
+        "var",
+        "log",
+        "splunk",
+        "deceptionlogic.log",
+    )
+    file_handler = logging.handlers.RotatingFileHandler(
+        log_file, maxBytes=25000000, backupCount=5
+    )
+    formatter = logging.Formatter("%(asctime)s %(levelname)s %(message)s")
     file_handler.setFormatter(formatter)
 
     logger.addHandler(file_handler)
     return logger
 
+
 ### MAIN FUNCTION ###
 
 if __name__ == "__main__":
-
     ## CHECK For the deceptionlogic.json file exists ##
     filename = "deceptionlogic.json"
     jsonfile = os.path.join(sys.path[0], filename)
     try:
-        with open(jsonfile, 'r') as argfile:
+        with open(jsonfile, "r") as argfile:
             data = argfile.read()
-    except:
+    except Exception:
         logger = setup_logger(logging.ERROR)
         logger.error("Alert Error: Deceptionlogic args file missing : ./%s ", jsonfile)
         sys.exit()
@@ -48,54 +62,75 @@ if __name__ == "__main__":
         sys.exit()
 
     if ("X-DeceptionLogic-KeyId" in args) and ("X-DeceptionLogic-SecretKey" in args):
-        keyId = str(args['X-DeceptionLogic-KeyId'])
-        SecretKey = str(args['X-DeceptionLogic-SecretKey'])
+        keyId = str(args["X-DeceptionLogic-KeyId"])
+        SecretKey = str(args["X-DeceptionLogic-SecretKey"])
     else:
         logger = setup_logger(logging.ERROR)
-        logger.error("Alert Error: Deceptionlogic args X-DeceptionLogic-KeyId OR/AND X-DeceptionLogic-SecretKey missing in file : ./%s ", jsonfile)
+        logger.error(
+            "Alert Error: Deceptionlogic args X-DeceptionLogic-KeyId OR/AND X-DeceptionLogic-SecretKey missing in file : ./%s ",  # noqa: E501
+            jsonfile,
+        )
         sys.exit()
 
-    if (keyId and SecretKey):
+    if keyId and SecretKey:
         headers = {
-            'X-DeceptionLogic-KeyId': keyId,
-            'X-DeceptionLogic-SecretKey': SecretKey,
+            "X-DeceptionLogic-KeyId": keyId,
+            "X-DeceptionLogic-SecretKey": SecretKey,
         }
 
-        response = requests.get('https://api.deceptionlogic.com/v0/authenticate', headers=headers, verify=False)
+        response = requests.get(
+            "https://api.deceptionlogic.com/v0/authenticate",
+            headers=headers,
+            verify=False,
+        )
         if response.status_code == 200:
             logger = setup_logger(logging.INFO)
             logger.info("Alert Info: DeceptionLogic API Authentication Success")
         else:
             logger = setup_logger(logging.ERROR)
-            logger.error("Alert Error: DeceptionLogic API Authentication Failed. Response Code:%s ", response.status_code)
+            logger.error(
+                "Alert Error: DeceptionLogic API Authentication Failed. Response Code:%s ",  # noqa: E501
+                response.status_code,
+            )
             sys.exit()
 
         auth = response.json()
-        token = auth['token']
-        api_id = auth['id']
+        token = auth["token"]
+        api_id = auth["id"]
 
         headers = {
-            'X-DeceptionLogic-Token': token,
-            'X-DeceptionLogic-Id': api_id,
+            "X-DeceptionLogic-Token": token,
+            "X-DeceptionLogic-Id": api_id,
         }
 
         if "alertapi_run_time" in args:
-            runtime = str(args['alertapi_run_time'])
+            runtime = str(args["alertapi_run_time"])
 
         if runtime is None:
-            runtime = '1m' ## Default value of runtime 1m
+            runtime = "1m"  ## Default value of runtime 1m
 
-        if runtime[-1] == "m" or runtime[-1] == "s" or runtime[-1] == "h" or runtime[-1] == "d":
+        if (
+            runtime[-1] == "m"
+            or runtime[-1] == "s"
+            or runtime[-1] == "h"
+            or runtime[-1] == "d"
+        ):
             if runtime[:-1].isdigit():
-                url = 'https://api.deceptionlogic.com/v0/alerts?from={0}'.format(runtime)
+                url = "https://api.deceptionlogic.com/v0/alerts?from={0}".format(
+                    runtime
+                )
                 response = requests.get(url, headers=headers, verify=False)
             else:
                 logger = setup_logger(logging.ERROR)
-                logger.error("Alert Error: Please enter the alertapi_run_time in deceptionlogic.json in right format (digits + m for months, d for days, s for seconds and h for hours). E.g for 1 min : 1m")
+                logger.error(
+                    "Alert Error: Please enter the alertapi_run_time in deceptionlogic.json in right format (digits + m for months, d for days, s for seconds and h for hours). E.g for 1 min : 1m"  # noqa: E501
+                )
                 sys.exit()
         else:
             logger = setup_logger(logging.ERROR)
-            logger.error("Alert Error: Please enter the alertapi_run_time in deceptionlogic.json in right format (digits + m for months, d for days, s for seconds and h for hours). E.g for 1 min : 1m")
+            logger.error(
+                "Alert Error: Please enter the alertapi_run_time in deceptionlogic.json in right format (digits + m for months, d for days, s for seconds and h for hours). E.g for 1 min : 1m"  # noqa: E501
+            )
             sys.exit()
 
         try:
@@ -106,14 +141,26 @@ if __name__ == "__main__":
                     date_time = i["date_time"]
                     del i["date_time"]
                     data_j = json.dumps(i)
-                    data_j = data_j[:1] + "\"date\": \"" + date_time + "\", \"" + data_j[2:] + "\n"
+                    data_j = (
+                        data_j[:1]
+                        + '"date": "'
+                        + date_time
+                        + '", "'
+                        + data_j[2:]
+                        + "\n"
+                    )
                     print(data_j)
 
         except ValueError:
             logger = setup_logger(logging.ERROR)
-            logger.error("Alert API call failed . Please check your authentication key or check with DeceptionLogic Support team. API response code: %s", response.status_code)
+            logger.error(
+                "Alert API call failed . Please check your authentication key or check with DeceptionLogic Support team. API response code: %s",  # noqa: E501
+                response.status_code,
+            )
             sys.exit()
     else:
         logger = setup_logger(logging.ERROR)
-        logger.error("DeceptionLogic API key ID and Secret Key can not be blank. Please Enter the right keys")
+        logger.error(
+            "DeceptionLogic API key ID and Secret Key can not be blank. Please Enter the right keys"  # noqa: E501
+        )
         sys.exit()
--- splunk_ta_deceptionlogic/bin/spl_deceptionlogic_events.py
+++ splunk_ta_deceptionlogic/bin/spl_deceptionlogic_events.py
@@ -1,6 +1,6 @@
-'''
+"""
 Send Deception Logic events to Splunk
-'''
+"""
 import os
 import sys
 import json
@@ -10,32 +10,46 @@ import requests
 
 
 def setup_logger(level):
-    '''
+    """
     WRITE THE INTERNAL LOGS TO LOGFILE FOR DECEPTIONLOGIC
-    '''
-    logger = logging.getLogger('')
-    logger.propagate = False # Prevent the log messages from being duplicated in the python.log file
+    """
+    logger = logging.getLogger("")
+    logger.propagate = (
+        False  # Prevent the log messages from being duplicated in the python.log file
+    )
     logger.setLevel(level)
-    log_file = os.path.join(sys.path[0], "..", "..", "..", "..", 'var', 'log', 'splunk', 'deceptionlogic.log')
-    file_handler = logging.handlers.RotatingFileHandler(log_file, maxBytes=25000000, backupCount=5)
-    formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+    log_file = os.path.join(
+        sys.path[0],
+        "..",
+        "..",
+        "..",
+        "..",
+        "var",
+        "log",
+        "splunk",
+        "deceptionlogic.log",
+    )
+    file_handler = logging.handlers.RotatingFileHandler(
+        log_file, maxBytes=25000000, backupCount=5
+    )
+    formatter = logging.Formatter("%(asctime)s %(levelname)s %(message)s")
     file_handler.setFormatter(formatter)
 
     logger.addHandler(file_handler)
     return logger
 
+
 ### MAIN FUNCTION ###
 
 if __name__ == "__main__":
-
     ## CHECK For the deceptionlogic.json file exists ##
     filename = "deceptionlogic.json"
     jsonfile = os.path.join(sys.path[0], filename)
 
     try:
-        with open(jsonfile, 'r') as argfile:
+        with open(jsonfile, "r") as argfile:
             data = argfile.read()
-    except:
+    except Exception:
         logger = setup_logger(logging.ERROR)
         logger.error("Events Error: Deceptionlogic args file missing : ./%s ", jsonfile)
         sys.exit()
@@ -49,54 +63,75 @@ if __name__ == "__main__":
         sys.exit()
 
     if ("X-DeceptionLogic-KeyId" in args) and ("X-DeceptionLogic-SecretKey" in args):
-        keyId = str(args['X-DeceptionLogic-KeyId'])
-        SecretKey = str(args['X-DeceptionLogic-SecretKey'])
+        keyId = str(args["X-DeceptionLogic-KeyId"])
+        SecretKey = str(args["X-DeceptionLogic-SecretKey"])
     else:
         logger = setup_logger(logging.ERROR)
-        logger.error("Events Error: Deceptionlogic args X-DeceptionLogic-KeyId OR/AND X-DeceptionLogic-SecretKey missing in file : ./%s ", jsonfile)
+        logger.error(
+            "Events Error: Deceptionlogic args X-DeceptionLogic-KeyId OR/AND X-DeceptionLogic-SecretKey missing in file : ./%s ",  # noqa: E501
+            jsonfile,
+        )
         sys.exit()
 
-    if (keyId and SecretKey):
+    if keyId and SecretKey:
         headers = {
-            'X-DeceptionLogic-KeyId': keyId,
-            'X-DeceptionLogic-SecretKey': SecretKey,
+            "X-DeceptionLogic-KeyId": keyId,
+            "X-DeceptionLogic-SecretKey": SecretKey,
         }
 
-        response = requests.get('https://api.deceptionlogic.com/v0/authenticate', headers=headers, verify=False)
+        response = requests.get(
+            "https://api.deceptionlogic.com/v0/authenticate",
+            headers=headers,
+            verify=False,
+        )
         if response.status_code == 200:
             logger = setup_logger(logging.INFO)
             logger.info("Events INFO : DeceptionLogic API Authentication Success")
         else:
             logger = setup_logger(logging.ERROR)
-            logger.error("Events Error: DeceptionLogic API Authentication Failed. Response Code:%s ", response.status_code)
+            logger.error(
+                "Events Error: DeceptionLogic API Authentication Failed. Response Code:%s ",  # noqa: E501
+                response.status_code,
+            )
             sys.exit()
 
         auth = response.json()
-        token = auth['token']
-        apid_id = auth['id']
+        token = auth["token"]
+        apid_id = auth["id"]
 
         headers = {
-            'X-DeceptionLogic-Token': token,
-            'X-DeceptionLogic-Id': apid_id,
+            "X-DeceptionLogic-Token": token,
+            "X-DeceptionLogic-Id": apid_id,
         }
 
         if "eventapi_run_time" in args:
-            runtime = str(args['eventapi_run_time'])
+            runtime = str(args["eventapi_run_time"])
 
         if runtime is None:
-            runtime = '1m' ## Default value of runtime 1m
+            runtime = "1m"  ## Default value of runtime 1m
 
-        if runtime[-1] == "m" or runtime[-1] == "s" or  runtime[-1] == "h" or runtime[-1] == "d":
+        if (
+            runtime[-1] == "m"
+            or runtime[-1] == "s"
+            or runtime[-1] == "h"
+            or runtime[-1] == "d"
+        ):
             if runtime[:-1].isdigit():
-                url = 'https://api.deceptionlogic.com/v0/events?from={0}'.format(runtime)
+                url = "https://api.deceptionlogic.com/v0/events?from={0}".format(
+                    runtime
+                )
                 response = requests.get(url, headers=headers, verify=False)
             else:
                 logger = setup_logger(logging.ERROR)
-                logger.error("Events Error: Please enter the eventapi_run_time in deceptionlogic.json in right format (digits + m for months, d for days, s for seconds and h for hours). E.g for 1 min : 1m")
+                logger.error(
+                    "Events Error: Please enter the eventapi_run_time in deceptionlogic.json in right format (digits + m for months, d for days, s for seconds and h for hours). E.g for 1 min : 1m"  # noqa: E501
+                )
                 sys.exit()
         else:
             logger = setup_logger(logging.ERROR)
-            logger.error("Events Error: Please enter the eventapi_run_time in deceptionlogic.json in right format (digits + m for months, d for days, s for seconds and h for hours). E.g for 1 min : 1m")
+            logger.error(
+                "Events Error: Please enter the eventapi_run_time in deceptionlogic.json in right format (digits + m for months, d for days, s for seconds and h for hours). E.g for 1 min : 1m"  # noqa: E501
+            )
             sys.exit()
 
         try:
@@ -109,9 +144,14 @@ if __name__ == "__main__":
 
         except ValueError:
             logger = setup_logger(logging.ERROR)
-            logger.error("Events API call failed . Please check your authentication key or check with DeceptionLogic Support team. API response code : %s", response.status_code)
+            logger.error(
+                "Events API call failed . Please check your authentication key or check with DeceptionLogic Support team. API response code : %s",  # noqa: E501
+                response.status_code,
+            )
             sys.exit()
     else:
         logger = setup_logger(logging.ERROR)
-        logger.error("DeceptionLogic API key ID and Secret Key can not be blank. Please Enter the right keys")
+        logger.error(
+            "DeceptionLogic API key ID and Secret Key can not be blank. Please Enter the right keys"  # noqa: E501
+        )
         sys.exit()
